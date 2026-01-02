bc73f3730dc195e1964118fe444fe5c107243f6f
ThirstyMango <vojtaraja@seznam.cz>
2026-01-02 19:49:23 +0100

    Sketch out the object oriented implementation

--- main.js
+++ main.js
@@ -1,10 +1,144 @@
-import { DOM } from "./scripts/DOM.js";
-import { View } from "./scripts/View.js";
-import { Library } from "./scripts/Library.js";
+class Book {
+  constructor(name, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
 
-const myDom = new DOM();
-const myView = new View();
-const myLibrary = new Library();
+class Library {
+  #data = [];
+
+  constructor(...data) {
+    for (arg of arguments) {
+      this.#data.push(arg);
+    }
+  }
+
+  findById(id) {
+    return this.data.find((item) => item.id === item);
+  }
+
+  addToLibrary(item) {
+    this.data.push(item);
+  }
+
+  removeFromLibrary(id) {
+    this.data = this.data.filter((item) => item.id !== id);
+  }
+}
+
+class DOM {
+  constructor() {
+    this.cacheDom();
+    this.bindEvents();
+  }
+
+  cacheDom() {
+    this.table = document.querySelector(".table");
+    this.library = document.getElementById("library");
+    this.btnAdd = document.getElementById("btnAdd");
+    this.inputRow = document.getElementById("input-row");
+    this.addRow = document.getElementById("add-row");
+    this.inputId = document.getElementById("input-id");
+    this.inputName = document.getElementById("input-name");
+    this.inputAuthor = document.getElementById("input-author");
+    this.inputPages = document.getElementById("input-pages");
+    this.inputRead = document.getElementById("input-read");
+    this.messager = document.querySelector(".message");
+    this.builder = this.#builder();
+  }
+
+  // TODO - bind event listeners
+  bindEvents() {}
+
+  // TODO - manage form handling
+  isFormDataValid() {}
+
+  #builder() {
+    const buildCheckboxEl = (value, id) => {
+      const checkboxEl = document.createElement("input");
+      checkboxEl.classList.add("checkbox");
+      checkboxEl.type = "checkbox";
+      checkboxEl.dataset.element = "read";
+      checkboxEl.name = `read_${id}`;
+      checkboxEl.checked = value;
+      return checkboxEl;
+    };
+
+    const buildRemoveBtnEl = () => {
+      const removeBtnEl = document.createElement("button");
+      removeBtnEl.setAttribute("type", "button");
+      removeBtnEl.dataset.element = "remove";
+      removeBtnEl.classList.add("btn", "btn--alert");
+      removeBtnEl.textContent = String.fromCodePoint(0x2715);
+      return removeBtnEl;
+    };
+
+    const buildBookEl = (book) => {
+      const tr = document.createElement("tr");
+      tr.classList.add("table__row");
+      tr.setAttribute("data-id", book.id);
+
+      for (const [key, value] of Object.entries(book)) {
+        const td = document.createElement("td");
+
+        // Create the inside of td
+        switch (key) {
+          case "read":
+            const checkboxEl = this.buildcheckboxElement(value, book.id);
+            td.appendChild(checkboxEl);
+            break;
+          default:
+            td.textContent = value;
+        }
+
+        tr.appendChild(td);
+      }
+
+      const tdAction = document.createElement("td");
+      const removeBtnEl = this.buildRemoveButtonElement();
+      tdAction.appendChild(removeBtnEl);
+
+      tr.appendChild(tdAction);
+
+      return tr;
+    };
+
+    return { buildCheckboxEl, buildRemoveBtnEl, buildBookEl };
+  }
+
+  renderBooks(...books) {
+    for (const book of books) {
+      const bookEl = this.builder.buildBookEl(book);
+      this.library.appendChild(bookEl);
+    }
+  }
+
+  renderMessage(message, success = true) {
+    this.messager.textContent = message;
+    success
+      ? this.messager.classList.add("message--success")
+      : this.messager.classList.add("message--alert");
+    this.messager.classList.remove("hidden");
+    setTimeout(() => this.hideMessage(), "2000");
+  }
+
+  hideMessage() {
+    this.messager.classList.remove("message--success");
+    this.messager.classList.remove("message--alert");
+    this.messager.classList.add("hidden");
+  }
+
+  #findBookElFromChild = function (childEl) {
+    if (childEl.dataset.id) {
+      return childEl;
+    }
+    return this.findBookElFromChild(childEl.parentNode);
+  };
+}
 
 const testingData = [
   ["Hobit", "J.R.R. Tolkien", 219, true, 8],
--- scripts/Book.js
+++
@@ -1,9 +0,0 @@
-function Book(name, author, pages, read) {
-  this.id = crypto.randomUUID();
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-export { Book };
