cd61a8c76058ebfb5fd25873c8f14e255ea2b504
ThirstyMango <vojtaraja@seznam.cz>
2026-01-04 14:23:57 +0100

    Rewrite the code into classes

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ class Library {
   // an array of { name, author, pages, read }
   constructor(...bookData) {
     for (const packet of bookData) {
-      const book = new Book(...packet.values());
+      const book = new Book(...Object.values(packet));
       this.#books.push(book);
     }
   }
@@ -30,22 +30,50 @@ class Library {
 
   // bookData = {name, author, pages, read}
   addBook(bookData) {
-    const book = new Book(...bookData.values());
+    if (!this.#isBookDataValid(...Object.values(bookData))) {
+      throw new Error("Book information invalid");
+    }
+
+    const book = new Book(...Object.values(bookData));
     this.#books.push(book);
+    return book;
   }
 
   removeBook(id) {
     this.#books = this.#books.filter((book) => book.id !== id);
   }
+
+  setBookParam(id, key, value) {
+    const book = this.findBook(id);
+    book[key] = value;
+  }
+
+  #isBookDataValid(name, author, pages, read) {
+    const isNameValid = name.length > 0;
+    const isAuthorValid = author.length > 0;
+    const isReadValid = typeof read === "boolean";
+    const isPagesValid =
+      pages !== "" &&
+      pages !== " " &&
+      !isNaN(pages) &&
+      Number.isInteger(parseFloat(pages));
+
+    return isNameValid && isAuthorValid && isReadValid && isPagesValid;
+  }
 }
 
 // DOM manipulation, rendering, event listeners...
 class DOM {
+  #timeout;
   constructor(library) {
     this.cacheDom();
     this.bindEvents();
     this.builder = this.#builder();
+    this.eventHandler = this.#eventHandler();
     this.library = library;
+
+    //Innit render
+    this.renderLibrary();
   }
 
   cacheDom() {
@@ -62,11 +90,11 @@ class DOM {
     this.messagerEl = document.querySelector(".message");
   }
 
-  // TODO - bind event listeners
-  bindEvents() {}
-
-  // TODO - manage form handling
-  isFormDataValid() {}
+  bindEvents() {
+    this.tableEl.addEventListener("click", (e) =>
+      this.eventHandler.handleClick(e)
+    );
+  }
 
   #builder() {
     const buildCheckboxEl = (value, id) => {
@@ -99,7 +127,7 @@ class DOM {
         // Create the inside of td
         switch (key) {
           case "read":
-            const checkboxEl = this.buildcheckboxElement(value, book.id);
+            const checkboxEl = buildCheckboxEl(value, book.id);
             td.appendChild(checkboxEl);
             break;
           default:
@@ -110,7 +138,7 @@ class DOM {
       }
 
       const tdAction = document.createElement("td");
-      const removeBtnEl = this.buildRemoveButtonElement();
+      const removeBtnEl = buildRemoveBtnEl();
       tdAction.appendChild(removeBtnEl);
 
       tr.appendChild(tdAction);
@@ -121,11 +149,89 @@ class DOM {
     return { buildCheckboxEl, buildRemoveBtnEl, buildBookEl };
   }
 
+  #eventHandler() {
+    const handleClick = (e) => {
+      const targetType = e.target.dataset.element;
+      switch (targetType) {
+        case "read":
+          handleReadClick(e.target);
+          break;
+        case "remove":
+          handleRemoveClick(e.target);
+          break;
+        case "confirm":
+          handleConfirmClick(e);
+          break;
+        case "add":
+          handleAddClick();
+          break;
+        case "cancel":
+          handleCancelClick();
+          break;
+      }
+    };
+
+    const handleRemoveClick = (targetEl) => {
+      const bookEl = targetEl.closest("tr");
+      const bookId = bookEl.dataset.id;
+      const book = this.library.findBook(bookId);
+
+      if (
+        !confirm(
+          `Do you wish to remove ${book.name} by ${book.author} from the library?`
+        )
+      )
+        return;
+
+      this.library.removeBook(bookId);
+      bookEl.remove();
+      this.renderMessage("Succesfully removed", true);
+    };
+
+    const handleReadClick = (targetEl) => {
+      const bookEl = targetEl.closest("tr");
+      const bookId = bookEl.dataset.id;
+      this.library.setBookParam(bookId, "read", targetEl.checked);
+    };
+
+    const handleConfirmClick = (e) => {
+      e.preventDefault();
+      const newBookData = this.#getFormData();
+      try {
+        const newBook = this.library.addBook(newBookData);
+        this.renderBook(newBook);
+      } catch (err) {
+        this.renderMessage(err.message, false);
+        return;
+      }
+
+      this.#clearFormData();
+      this.renderMessage("Book added", true);
+      this.hideEl(this.inputRowEl);
+      this.showEl(this.addRowEl);
+    };
+
+    const handleAddClick = () => {
+      this.showEl(this.inputRowEl);
+      this.hideEl(this.addRowEl);
+    };
+
+    const handleCancelClick = () => {
+      this.showEl(this.addRowEl);
+      this.hideEl(this.inputRowEl);
+      this.#clearFormData();
+    };
+
+    return { handleClick };
+  }
+
   renderLibrary() {
-    for (const book of this.library.books) {
-      const bookEl = this.builder.buildBookEl(book);
-      this.libraryEl.appendChild(bookEl);
-    }
+    this.library.books.forEach((book) => this.renderBook(book));
+  }
+
+  renderBook(book) {
+    const bookEl = this.builder.buildBookEl(book);
+    this.libraryEl.appendChild(bookEl);
   }
 
   renderMessage(message, success = true) {
@@ -133,15 +239,22 @@ class DOM {
     const el = this.messagerEl;
     const stateCls = success ? successCls : alertCls;
 
+    // Cancel the previous hiding interval and reset the state of messagerEl
+    clearInterval(this.#timeout);
+    this.removeClasses(el, successCls, alertCls);
+
+    // Display the message
     this.setContent(el, message);
     this.addClasses(el, stateCls);
     this.showEl(el);
 
-    // Hide the message and remove classes after 2s
-    setTimeout(() => {
+    // Hide after 3s
+    this.hideEl(el);
+    this.removeClasses(el, successCls, alertCls);
+    this.#timeout = setTimeout(() => {
       this.hideEl(el);
       this.removeClasses(el, successCls, alertCls);
-    }, "2000");
+    }, "3000");
   }
 
   showEl(el) {
@@ -157,21 +270,30 @@ class DOM {
   }
 
   removeClasses(el, ...classes) {
-    el.classList.remove(classes);
+    el.classList.remove(...classes);
   }
 
   addClasses(el, ...classes) {
-    el.classList.add(classes);
+    el.classList.add(...classes);
   }
 
-  #findBookElFromChild = function (childEl) {
-    if (childEl.dataset.id) {
-      return childEl;
-    }
-    return this.findBookElFromChild(childEl.parentNode);
-  };
+  #getFormData() {
+    const name = this.inputNameEl.value;
+    const author = this.inputAuthorEl.value;
+    const pages = this.inputPagesEl.value;
+    const read = this.inputReadEl.checked;
+    return [name, author, pages, read];
+  }
+
+  #clearFormData() {
+    this.inputNameEl.value = "";
+    this.inputAuthorEl.value = "";
+    this.inputPagesEl.value = "";
+    this.inputReadEl.checked = false;
+  }
 }
 
+// Adding initial data
 const testingData = [
   { name: "Hobit", author: "J.R.R. Tolkien", pages: 219, read: true },
   {
@@ -184,110 +306,6 @@ const testingData = [
   { name: "Golem", author: "Gustav Meyrink", pages: 331, read: false },
 ];
 
-myView.displayLibrary(myLibrary.data, myDom.library);
-
-// Helper functions
-const isFormDataValid = function isFormDataValid(name, author, pages, read) {
-  const isNameValid = name.length > 0;
-  const isAuthorValid = author.length > 0;
-  const isReadValid = typeof read === "boolean";
-  const isPagesValid =
-    pages !== "" &&
-    pages !== " " &&
-    !isNaN(pages) &&
-    Number.isInteger(parseFloat(pages));
-
-  return isNameValid && isAuthorValid && isReadValid && isPagesValid;
-};
-
-const getBookData = function getBookData() {
-  const name = myDom.inputName.value;
-  const author = myDom.inputAuthor.value;
-  const pages = myDom.inputPages.value;
-  const read = myDom.inputRead.checked;
-  return [name, author, pages, read];
-};
-
-const clearFormData = function clearFormData() {
-  myDom.inputName.value = "";
-  myDom.inputAuthor.value = "";
-  myDom.inputPages.value = "";
-  myDom.inputRead.checked = false;
-};
-
-// Event listeners
-
-const handleRemoveClick = function handleRemoveClick(targetEl) {
-  const bookEl = myDom.findBookElFromChild(targetEl);
-  const bookId = bookEl.dataset.id;
-  const book = myLibrary.findBookById(bookId);
-
-  if (
-    !confirm(
-      `Do you wish to remove ${book.name} by ${book.author} from the library?`
-    )
-  )
-    return;
-
-  myLibrary.removeBookFromLibrary(bookId);
-  bookEl.remove();
-  myView.displayTempMessage(myDom.messager, "Succesfully removed", false);
-};
-
-const handleReadClick = function handleReadClick(targetEl) {
-  const bookEl = myDom.findBookElFromChild(targetEl);
-  const bookId = bookEl.dataset.id;
-  myLibrary.setBookParameter(bookId, "read", targetEl.checked);
-};
-
-const handleConfirmClick = function handleConfirmClick(e) {
-  const newBookData = getBookData();
-
-  if (!isFormDataValid(...newBookData)) {
-    return;
-  }
-
-  e.preventDefault();
-  clearFormData();
-  const newBook = myLibrary.addBookToLibrary(...newBookData);
-  myView.displayBook(newBook, myDom.library);
-  myView.displayTempMessage(myDom.messager, "Succesfully created", true);
-
-  myDom.inputRow.classList.add("hidden");
-  myDom.addRow.classList.remove("hidden");
-};
-
-const handleAddClick = function handleAddClick() {
-  myDom.inputRow.classList.remove("hidden");
-  myDom.addRow.classList.add("hidden");
-};
-
-const handleCancelClick = function handleCancelClick() {
-  myDom.inputRow.classList.add("hidden");
-  myDom.addRow.classList.remove("hidden");
-  clearFormData();
-};
-
-const handleClick = function handleClick(e) {
-  const targetType = e.target.dataset.element;
-  switch (targetType) {
-    case "read":
-      handleReadClick(e.target);
-      break;
-    case "remove":
-      handleRemoveClick(e.target);
-      break;
-    case "confirm":
-      handleConfirmClick(e);
-      break;
-    case "add":
-      handleAddClick();
-      break;
-    case "cancel":
-      handleCancelClick();
-      break;
-  }
-};
-
-// Add Event listener
-myDom.table.addEventListener("click", handleClick);
+// Classes construction
+const myLibrary = new Library(...testingData);
+const initDOM = new DOM(myLibrary);
